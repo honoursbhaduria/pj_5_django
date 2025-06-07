ada231c61bc0e28b23b88fd8fca51d011e1dc855
honours <honoursbhadauria@gmail.com>
2025-06-07 21:20:18 +0530

    Implement product model, views, and templates for product catalog

--- imageGallery/gallery/views.py
+++ imageGallery/gallery/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
+from django.http import HttpResponse
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+def home(request):
+    return httpResponse("hello world")
